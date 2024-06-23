1d2dfa7dff6318f20699f473f1c8f2bfb78b3f85
Sher Afgan <145536822+sherafgan313@users.noreply.github.com>
2024-06-23 00:17:55 -0700

    even or odd

---
+++ even or odd.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+	
+	int num;
+	
+	cout<<"Enter Number to find wether it is EVEN or ODD : ";
+	cin>>num;
+	
+	if(num%2==0)
+	
+	cout<<"The Given Number is EVEN"<<endl;
+	
+	else
+	cout<<"The Given Number is ODD"<<endl;
+	
+	return 0;
+}
